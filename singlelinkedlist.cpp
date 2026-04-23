46fad3178726a15d6166b8659b965eb4a49616df
suhilmi01 <suhilmi01@mail.com>
2026-04-23 20:23:15 +0700

    input output

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -2,17 +2,5 @@
 #include <string>
 using namespace std;
 
-class Node
-{
-public:
-    int noMhs;
-    Node *next;
-};
-class LinkedList
-{
-    Node *START;
 
-public:
-    LinkedList()
-    {
-        START = NULL;
+
